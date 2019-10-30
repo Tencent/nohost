3ed8e4a377e464c6b0cac1dac46858d5b0b4913b
avwo <avwu@qq.com>
2019-10-30 22:31:34 +0800

    update whistle

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -35,7 +35,7 @@ class NavBar extends Component {
     return (
       <div className="p-nav-bar" onClick={this.onChange}>
         <a href="https://github.com/imweb/punk" target="_blank" rel="noopener noreferrer" className="p-logo">
-          抓包调试及环境配置平台
+          Punk
         </a>
         <button
           type="button"
