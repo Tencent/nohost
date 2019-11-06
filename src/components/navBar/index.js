7bcd32881c711a2f37e3db115f8a70b036fccbf5
avwo <avwu@qq.com>
2019-11-06 23:40:11 +0800

    style: 修改仓库地址

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -34,7 +34,7 @@ class NavBar extends Component {
     const { active } = this.props;
     return (
       <div className="p-nav-bar" onClick={this.onChange}>
-        <a href="https://github.com/imweb/punk" target="_blank" rel="noopener noreferrer" className="p-logo">
+        <a href="https://github.com/nohosts/nohost" target="_blank" rel="noopener noreferrer" className="p-logo">
           Nohost
         </a>
         <button
@@ -74,7 +74,7 @@ class NavBar extends Component {
         >设置
         </button>
         <span className="p-help" onMouseEnter={this.showAboutMenu} onMouseLeave={this.hideAboutMenu}>
-          <a href="https://github.com/imweb/punk" rel="noopener noreferrer" target="_blank">
+          <a href="https://github.com/nohosts/nohost" rel="noopener noreferrer" target="_blank">
               帮助
             <Icon type="down" className="p-help-menu" />
           </a>
