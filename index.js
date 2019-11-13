048d0048f00982bf47644d98190ed2de4a86e245
avwo <avwu@qq.com>
2019-11-13 13:17:37 +0800

    refine code

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const fse = require('fs-extra');
 const { Socket } = require('net');
 const { getWhistlePath } = require('whistle/lib/config');
+const initConfig = require('./lib/config');
 // 避免第三方模块没处理好异常导致程序crash
 const noop = () => {};
+const DEFAULT_PORT = 8080;
 const destroySocket = Socket.prototype.destroy;
 Socket.prototype.destroy = function(err) {
   if (err && this.listenerCount('error')) {
@@ -15,4 +17,14 @@ Socket.prototype.destroy = function(err) {
 process.env.WHISTLE_PATH = process.env.NOHOST_PATH || getWhistlePath();
 fse.ensureDirSync(process.env.WHISTLE_PATH); // eslint-disable-line
 
-module.exports = require('./lib');
+module.exports = (options, cb) => {
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  } else if (!options) {
+    options = {};
+  }
+  options.port = parseInt(options.port, 10) || DEFAULT_PORT;
+  initConfig(options);
+  require('./lib')(options, cb); // eslint-disable-line
+};
