b36ea6a498d397a3b0a69952813ac46baa8116f6
avwo <avwu@qq.com>
2019-11-26 16:32:27 +0800

    docs: init

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:nohosts/nohost.git master:gh-pages
+
+cd -
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: 'Nohost',
+  description: 'Multi-user & multi-env web debugging proxy based on whistle',
+  base: '/nohost/',
+  port: 8081,
+  
+}
