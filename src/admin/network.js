c56c835cfef211f663c2e3e4cd64a225e77f0653
avwo <avwu@qq.com>
2020-03-25 12:57:05 +0800

    style: 支持输入提取码

--- src/admin/network.js
+++ src/admin/network.js
@@ -1,44 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { parse } from 'query-string';
+import { Modal, Button, Input } from 'antd';
 import Upload from '../components/upload';
 import { importSessions as getSessions } from './cgi';
 import { getString } from './util';
 import '../base.less';
 
-let { name, date, username } = parse(window.location.search);
+const ACCESS_CODE_RE = /^[a-z\d]{4}$/i;
+let { name, date, username, encrypted } = parse(window.location.search);
 name = getString(name);
 date = getString(date);
 username = getString(username);
 
 const clearNetwork = name ? '&clearNetwork=true' : '';
 
-const Network = () => {
-  return (
-    <div className="container fill vbox">
-      <Upload />
-      <iframe title="network" className="fill capture-win" src={`network/#network?ip=self${clearNetwork}`} />
-    </div>
-  );
-};
+class Network extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { disabled: true, visible: true };
+    window.onWhistleReady = (api) => {
+      this.api = api;
+      if (name) {
+        this.loadSessions();
+      }
+    };
+  }
 
-const init = () => {
-  window.onWhistleReady = (api) => {
-    if (!name) {
+  loadSessions = () => {
+    if (!this.api || (encrypted && !this.accessCode)) {
       return;
     }
     getSessions({
       name,
       date,
       username,
+      code: this.accessCode || '',
     }, (data) => {
-      if (!data) {
-        return;
+      if (data) {
+        this.api.importSessions(data);
       }
-      api.importSessions(data);
     });
-  };
-  ReactDOM.render(<Network />, document.getElementById('root'));
-};
+  }
 
-init();
+  onChange = (e) => {
+    const { value } = e.target;
+    this.setState({ value: value.replace(/\s+/g, ''), disabled: !ACCESS_CODE_RE.test(value) });
+  }
+
+  onConfirm = () => {
+    const { value } = this.state;
+    if (!ACCESS_CODE_RE.test(value)) {
+      return;
+    }
+    this.accessCode = value;
+    this.loadSessions();
+    this.setState({ visible: false });
+  };
+
+  render() {
+    const { disabled, value, visible } = this.state;
+    return (
+      <div className="container fill vbox">
+        {encrypted ? (
+          <Modal
+            visible={visible}
+            closable={false}
+            footer={[
+              <Button
+                onClick={this.onConfirm}
+                title={disabled ? '请输入四位提取码（不区分大小写）' : undefined}
+                disabled={disabled}
+                key="confirm"
+                type="primary"
+              >
+                确定
+              </Button>,
+            ]}
+          >
+            <Input value={value} onChange={this.onChange} placeholder="请输入四位提取码" maxLength={4} />
+          </Modal>
+        ) : null}
+        <Upload />
+        <iframe title="network" className="fill capture-win" src={`network/#network?ip=self${clearNetwork}`} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Network />, document.getElementById('root'));
