d8a5bd82fa2c6137a69c50a327c880209e56e1d6
avwo <avwu@qq.com>
2021-01-12 00:47:13 +0800

    chore: add license

--- bin/nohost.js
+++ bin/nohost.js
@@ -1,4 +1,13 @@
 #! /usr/bin/env node
+
+/**
+ * Tencent is pleased to support the open source community by making nohost-环境配置与抓包调试平台 available.
+ * Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved. The below software in
+ * this distribution may have been modified by THL A29 Limited ("Tencent Modifications").
+ * All Tencent Modifications are Copyright (C) THL A29 Limited.
+ * nohost-环境配置与抓包调试平台 is licensed under the MIT License except for the third-party components listed below.
+ */
+
 /* eslint no-console: "off" */
 const program = require('starting');
 const path = require('path');
