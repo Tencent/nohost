c127b3d1d8dab6452991ab65e86e39b0f66e821b
ZSI2017 <13407121461@163.com>
2021-04-08 21:25:48 +0800

    feat(button): 增加小圆点切换记录功能 (#62)

---
+++ src/button/envHistory.js
@@ -0,0 +1,77 @@
+/**
+ * 记录页面环境切换历史
+ * 目前最多记录5条
+ */
+import $ from 'jquery';
+import { safeParse, getLocalStorage, setLocalStorage } from './helper';
+import { LAST_SELECTED_ENV, ENV_HISTORY } from './const';
+
+/**
+ * 从缓存中获取切换记录
+ */
+export function getEnvHistory(check) {
+  let localValue = getLocalStorage(LAST_SELECTED_ENV);
+
+  if (!localValue) { return []; }
+
+  if (localValue[0] !== '[') {
+    // 兼容只保存了一个上次环境的早期情况
+    localValue = [localValue];
+  } else {
+    localValue = safeParse(localValue);
+  }
+
+  if (typeof check === 'function') {
+    localValue = localValue.filter((item) => {
+      return check(item);
+    });
+  }
+
+  return localValue || [];
+}
+
+/**
+ *  保存切换环境数据到localStorage，
+ *  最后切换的环境保存到最前面
+ */
+export function setEnvHistory(newValue) {
+  const envHistory = getEnvHistory();
+  const index = envHistory.indexOf(newValue);
+  if (index > -1) {
+    envHistory.splice(index, 1);
+  }
+
+  // 保证上一个切换的排在第一个
+  envHistory.unshift(newValue);
+
+  if (envHistory.length > ENV_HISTORY) {
+    envHistory.length = ENV_HISTORY;
+  }
+
+  setLocalStorage(LAST_SELECTED_ENV, JSON.stringify(envHistory));
+}
+
+/**
+ *  初始化切换历史的二级选择框内容html。
+ *  这里切换内容会随每次切换发生变化
+ *  所以需要每次切换都重新渲染
+ */
+export function initEnvHistoryDom() {
+  const envHistoryElement = $('#w-nohost-circle-history-list');
+
+  const envHistory = getEnvHistory();
+  if (envHistory.length < 1) { return; }
+
+  const newList = document.createDocumentFragment();
+
+  envHistory.forEach(item => {
+    const div = document.createElement('div');
+    div.className = 'w-nohost-second-item';
+    div.innerText = item;
+    div.setAttribute('value', item);
+
+    newList.appendChild(div);
+  });
+
+  envHistoryElement.html(newList);
+}
