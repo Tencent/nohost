c44c2d55544570aa8e3acc77ba7d0a618ffb2d3c
ZSI2017 <13407121461@163.com>
2021-04-08 21:25:48 +0800

    feat(button): 增加小圆点切换记录功能 (#62)

---
+++ src/button/const.js
@@ -0,0 +1,6 @@
+export const CUR_SELECTED_USER = '__nohost_-_selected_user_id__';
+export const LAST_SELECTED_ENV = '__nohost_-_last_selected_id__';
+
+
+// 切换记录最多保存数量
+export const ENV_HISTORY = 5;
